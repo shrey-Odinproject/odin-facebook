b874ae3c6dc20cc3d498eb92772546cef536aa60
shrey-Odinproject <sonishreyf1@gmail.com>
2022-08-04 19:25:21 +0530

    Allow users to send Friend Requests to other users
    doubt: Creation of relation instance should be done in
    Realtion_controller?, instead of user?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id]) if params[:id]
+    @user = User.find(params[:id])
   end
 
   def index
     @users = User.all
+  end
+
+  def create_request
+    # current_user.send_request(User.find(params[:id])) unable to use instance method
+    Request.create(friend_id: params[:id], user_id: current_user.id)
+    flash[:success] = 'Request sent!'
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "users#index"
-  resources :users, only: [:show, :index]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
+
+  resources :users, only: [:show, :index]
+
+  resources :users do
+    member do
+      post 'create_request'
+    end
+  end
 end
