944043d0ac20a6f7f53c089e48af109361e59af2
shrey-Odinproject <sonishreyf1@gmail.com>
2022-08-04 22:48:05 +0530

    Add button to accept friendship from other user 3 of 3 Create accept_request method which updates accepted flag of the request instance in question

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,16 @@ class UsersController < ApplicationController
     flash[:success] = 'Request sent!'
     redirect_to root_path
   end
+
+  def accept_request
+    @request = Request.where(friend_id: current_user.id, user_id: params[:requestor_id])[0]
+    @request.accepted = true
+    if @request.save
+      flash[:success] = 'Friendship Accepted!'
+      redirect_to root_path
+    else
+      flash.now[:error] = 'Could not be friends!'
+      render :show, status: :unprocessable_entity
+    end
+  end
 end
