1e01dc994b6f01aa68eba926cea60089dc2ecfc6
shrey-Odinproject <sonishreyf1@gmail.com>
2022-08-06 23:58:19 +0530

    Add logic for limiting Posts from getting more than 1 like from a single user

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,19 @@
 class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id]) # can use post_id as routing for likes controller is nested
-    @post.likes.create(user_id: current_user.id)
-    redirect_to post_path(@post)
+    if already_liked?
+      flash[:error] = 'U have already liked this post!'
+      # render post_path(@post), status: :unprocessable_entity
+    else
+      @post.likes.create(user_id: current_user.id)
+    end
+    redirect_to post_path(@post) # why is this not within if-else?
   end
+
+  private
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id: params[:post_id]).exists?
+  end
+  # the 2 post ids are for 1) column in likes table, 2) present in params dut to nested routes
 end
