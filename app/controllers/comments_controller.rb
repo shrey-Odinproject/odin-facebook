c780d7b216c181ac26083eada170524d49d9b379
shrey-Odinproject <sonishreyf1@gmail.com>
2022-08-07 13:42:24 +0530

    Add feature: create comments on a post by logged_in user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      flash[:success] = 'Comment created'
+    else
+      flash[:error] = 'error'
+    end
+    redirect_to post_path(params[:post_id])
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
