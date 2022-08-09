a2c7d0fb61bdf0218ed1c07ed5e54af317f68be5
shrey-Odinproject <sonishreyf1@gmail.com>
2022-08-09 17:07:32 +0530

    Shift gravatr profile img logic to profile from user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,2 @@
 module UsersHelper
-  def user_gravatar(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase) # get img hash for email
-    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}" # create url for image using hash
-    image_tag(gravatar_url, alt: user.name) # return an image tag to be placed on a view
-
-    # read section under the devloper resources->gravatr urls->gravatar image requests
-  end
 end
