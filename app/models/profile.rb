50436d9a2e34f2ad8d198dbe19aff3879ce7e864
shrey-Odinproject <sonishreyf1@gmail.com>
2022-08-09 16:50:39 +0530

    Remove strange custom error message

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,5 +2,5 @@ class Profile < ApplicationRecord
   belongs_to :user
 
   validates :gender, presence: true
-  validates :age, presence: true, numericality: { greater_than_or_equal_to: 13, less_than_or_equal_to: 120, message: 'Is not realistic' }
+  validates :age, presence: true, numericality: { greater_than_or_equal_to: 13, less_than_or_equal_to: 120 }
 end
