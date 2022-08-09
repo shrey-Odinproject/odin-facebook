dffa287e38cee82a6c5285b779f4d18f1819e395
shrey-Odinproject <sonishreyf1@gmail.com>
2022-08-09 14:31:22 +0530

    Add routes to create a new profile

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,6 @@ Rails.application.routes.draw do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
   end
+
+  resources :profiles, only: [:new, :create]
 end
