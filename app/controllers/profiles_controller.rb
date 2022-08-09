73ec41beadb03795d5f92e9bdcc203a466410dd7
shrey-Odinproject <sonishreyf1@gmail.com>
2022-08-09 14:46:57 +0530

    Add feature : user can create a profile now using form on profile#new

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,4 +3,21 @@ class ProfilesController < ApplicationController
     @profile = Profile.new
     @gender_options = ['male', 'female'].map {|g| [g, g] }
   end
+
+  def create
+    @profile = current_user.build_profile(profile_params)
+    
+    if @profile.save
+      flash[:success] = 'Profile created'
+      redirect_to current_user
+    else
+      flash.now[:error] = 'Error!'
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+  
+  def profile_params
+    params.require(:profile).permit(:gender, :age)
+  end
 end
