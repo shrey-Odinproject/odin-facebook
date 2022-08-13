17343672d5068b8c6c288cbb7b5c5f3e44a24d2f
shrey-Odinproject <sonishreyf1@gmail.com>
2022-08-14 00:21:22 +0530

    Add method to create a user after callback (if user doesnt exist else return existing ), uses email to find user

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,18 @@ class User < ApplicationRecord
   def send_request(user)
     Request.create(friend_id: user.id)
   end
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+        user = User.create(name: data['name'],
+           email: data['email'],
+           password: Devise.friendly_token[0,20]
+        )
+    end
+    user
+  end
 end
