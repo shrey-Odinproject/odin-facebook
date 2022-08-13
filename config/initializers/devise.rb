6c0cfa338a829dd4517c7d88fb6d43e90c3360f0
shrey-Odinproject <sonishreyf1@gmail.com>
2022-08-14 01:43:17 +0530

    Fix the issue of github sending blank emails/no emails back to the api call by adding scope

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, ENV['app_id'], ENV['app_secret']
+  config.omniauth :github, ENV['app_id'], ENV['app_secret'], scope: "user"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
