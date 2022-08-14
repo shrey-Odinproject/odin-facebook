95df9d31bbd1da6e8fd125fc6dd4f95a261ca9e4
shrey-Odinproject <sonishreyf1@gmail.com>
2022-08-14 22:56:54 +0530

    Add welcome_email method to user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,8 @@
 class UserMailer < ApplicationMailer
+  default from: 'odin@railsFinal.com'
+
+  def welcome_email
+    @user = params[:user]
+    mail(to: @user.email, subject: 'Welcome to My Site')
+  end
 end
