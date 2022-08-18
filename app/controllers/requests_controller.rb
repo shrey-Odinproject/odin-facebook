5468b2d077ecd582be4c090a8aa1792855f3c6a0
shrey-Odinproject <sonishreyf1@gmail.com>
2022-08-18 14:32:22 +0530

    Create the methods to send(create), accept(update), decline(destroy) requests in requests_controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,23 @@
 class RequestsController < ApplicationController
+
+  def create
+    Request.create(friend_id: params[:friend_id], user_id: current_user.id)
+    flash[:success] = 'Request sent!'
+    redirect_to root_path
+  end
+
+  def update
+    # acceptin a request is updating the already existing record
+    @request = Request.find(params[:request_id])
+    @request.update(accepted: true)
+    flash[:success] = 'Friendship Accepted!'
+    redirect_to current_user
+  end
+
+  def destroy
+    @request = Request.find(params[:request_id])
+    @request.destroy
+    flash[:success] = 'Friendship Declined!'
+    redirect_to current_user
+  end
 end
