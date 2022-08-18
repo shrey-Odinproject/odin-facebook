e83d0ebe5def86ca7b0c360373574a7cb3951fb1
shrey-Odinproject <sonishreyf1@gmail.com>
2022-08-18 14:29:50 +0530

    Remove request creation,accepting and declining methods from users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,34 +6,4 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-
-  def create_request
-    # current_user.send_request(User.find(params[:id])) unable to use instance method
-    Request.create(friend_id: params[:id], user_id: current_user.id)
-    flash[:success] = 'Request sent!'
-    redirect_to root_path
-  end
-
-  def accept_request
-    @request = Request.where(friend_id: current_user.id, user_id: params[:requestor_id])[0]
-    @request.accepted = true
-    if @request.save
-      flash[:success] = 'Friendship Accepted!'
-      redirect_to root_path
-    else
-      flash.now[:error] = 'Could not be friends!'
-      render :show, status: :unprocessable_entity
-    end
-  end
-
-  def decline_request
-    @request = Request.where(friend_id: current_user.id, user_id: params[:requestor_id])[0]
-    if @request.delete
-      flash[:success] = 'Friendship Declined!'
-      redirect_to root_path
-    else
-      flash.now[:error] = 'Could not decline!'
-      render :show, status: :unprocessable_entity
-    end
-  end
 end
