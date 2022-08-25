de110f083ff76751e7f51f7fce58f1fe2b1d86c2
shrey-Odinproject <sonishreyf1@gmail.com>
2022-08-25 22:38:23 +0530

    Make comments polymorphic on model level and associate to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
-  has_many :comments, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 3 }
