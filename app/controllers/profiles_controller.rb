d10d07cd1e8af60bd66c5e05660686d23fb696c2
shrey-Odinproject <sonishreyf1@gmail.com>
2022-08-26 00:06:44 +0530

    Change displaying profile details in user#_profile logic directly 1 of 2
    Create profile#show, we will display this show in user#_profile using
    turbo frame

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,7 @@ class ProfilesController < ApplicationController
 
   def create
     @profile = current_user.build_profile(profile_params)
-    
+
     if @profile.save
       flash[:success] = 'Profile created'
       redirect_to current_user
@@ -37,9 +37,12 @@ class ProfilesController < ApplicationController
     redirect_to current_user, status: :see_other
   end
 
+  def show
+    @profile = Profile.find(params[:id])
+  end
 
   private
-  
+
   def profile_params
     params.require(:profile).permit(:gender, :age)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
 
-  resources :profiles, except: [:inedx, :show]
+  resources :profiles, except: [:inedx]
 end
