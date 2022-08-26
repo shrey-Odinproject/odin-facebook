8833354df1084343f1b59c010332bf665f75dc79
shrey-Odinproject <sonishreyf1@gmail.com>
2022-08-26 16:23:35 +0530

    Add Feature: Allow uploading image when creating post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, as: :likeable, dependent: :destroy
   has_many :comments, as: :commentable, dependent: :destroy
+  has_one_attached :image, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 3 }
